ba30cb30b41bfa65c7a9d43e2eed281817d7cf12
Maximilian Krauss <maximilian.krauss@share-now.com>
2020-06-17 12:40:50 +0200

    Adds relative timestamp

--- handler/root.js
+++ handler/root.js
@@ -2,12 +2,12 @@ const { sql } = require('./../helper')
 const { get } = require('got').default
 
 async function fetchSensorData () {
-  return sql`SELECT * from "sensor_data" ORDER BY alias ASC`
+  const results = await sql`SELECT * from "sensor_data" ORDER BY alias ASC`
+  return results.map(item => ({ ...item, lastUpdate: item.last_update.toISOString() }))
 }
 
 async function fetchCurrentWeather () {
   const result = await get('http://api.weatherstack.com/current?access_key=8443fba9b44316a1e3bd53689e8b106f&query=Berlin').json()
-  console.log(result)
   const { current: { temperature, humidity, weather_icons: [icon], weather_descriptions: [description] } } = result
   return {
     description,
